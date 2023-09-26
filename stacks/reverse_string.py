f83a8d9b70384c331abc98c5f9d53d6f5aac3c1e
Upendra Singhai <upendra.singhai@druva.com>
2023-09-26 15:02:33 +0530

    Updates

--- stacks/reverse_string.py
+++ stacks/reverse_string.py
@@ -1,18 +1,14 @@
-"""
-- Array need traversing of the entire array
-"""
-
 import stack
 
-string = "gninraeL nIdekniL htiw tol a nraeL"
+string = "!reerac a t'nsi soahc esuaceB :serutcurts ataD"
 reversed_string = ""
 s = stack.Stack()
 
-# for when you know how many iterations
+# `for` when you know how many iterations
 for c in string:
     s.push(c)
 
-# When you dont know how many iterations
+# `when` you don't know how many iterations
 while not s.is_empty():
     reversed_string += s.pop()
 print(reversed_string)
